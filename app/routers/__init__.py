18163f04ccb355703424289661659bd8c9265ef7
Twopercent <twopercent051@zohomail.eu>
2025-05-18 21:53:41 +0400

    fix

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -5,4 +5,4 @@ from ..utils import check_api_key
 
 router = APIRouter(dependencies=[Security(check_api_key)], prefix="/api")
 
-router.include_router(router=organizations_router, prefix="/organizations")
+router.include_router(router=organizations_router, prefix="/organizations", tags=["Организации"])
